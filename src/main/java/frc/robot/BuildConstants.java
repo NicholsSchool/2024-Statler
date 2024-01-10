3af130edb0ab9204a7f7624dfa186aa90d27a511
Tom Greier <tom.greier@gmail.com>
2024-01-09 22:22:00 -0500

    updated to 2024 release

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-LewZealand";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 1;
-  public static final String GIT_SHA = "4ae5fae5d2b57cc9873086d045284679600ae7a2";
-  public static final String GIT_DATE = "2024-01-09 16:36:46 EST";
-  public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-01-09 18:12:57 EST";
-  public static final long BUILD_UNIX_TIME = 1704841977912L;
-  public static final int DIRTY = 0;
+  public static final int GIT_REVISION = 2;
+  public static final String GIT_SHA = "36767f15291ab4c44bd4a8bc0b2513781ef86f46";
+  public static final String GIT_DATE = "2024-01-09 18:35:33 EST";
+  public static final String GIT_BRANCH = "demo_code";
+  public static final String BUILD_DATE = "2024-01-09 22:07:39 EST";
+  public static final long BUILD_UNIX_TIME = 1704856059597L;
+  public static final int DIRTY = 1;
 
   private BuildConstants() {}
 }
