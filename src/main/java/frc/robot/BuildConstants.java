8edc7d9f792ef9ba77b4e51a3cd41b2270c78268
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-01-15 13:28:05 -0500

    build constants is modified every build, needs commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-LewZealand";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 2;
-  public static final String GIT_SHA = "36767f15291ab4c44bd4a8bc0b2513781ef86f46";
-  public static final String GIT_DATE = "2024-01-09 18:35:33 EST";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "56b095ff9d8151c3c002096f3325ee71334621ea";
+  public static final String GIT_DATE = "2024-01-11 13:58:57 EST";
   public static final String GIT_BRANCH = "demo_code";
-  public static final String BUILD_DATE = "2024-01-09 22:07:39 EST";
-  public static final long BUILD_UNIX_TIME = 1704856059597L;
+  public static final String BUILD_DATE = "2024-01-15 13:27:19 EST";
+  public static final long BUILD_UNIX_TIME = 1705343239198L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
