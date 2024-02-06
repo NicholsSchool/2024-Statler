3e7712a3fabbe637c4caf11341decdbba09c3566
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-02-06 18:44:44 -0500

    tuned turning PID

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-LewZealand";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "69f3207444083ab599eae216c1149f70ec6f8bd1";
-  public static final String GIT_DATE = "2024-02-01 17:42:44 EST";
+  public static final int GIT_REVISION = 29;
+  public static final String GIT_SHA = "f7e93d886155fc9a76ffc037fb6a7c684f00d29c";
+  public static final String GIT_DATE = "2024-02-06 16:22:23 EST";
   public static final String GIT_BRANCH = "develop";
-  public static final String BUILD_DATE = "2024-02-06 16:18:05 EST";
-  public static final long BUILD_UNIX_TIME = 1707254285252L;
+  public static final String BUILD_DATE = "2024-02-06 18:41:43 EST";
+  public static final long BUILD_UNIX_TIME = 1707262903452L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
