dc2b2091f4b2cad79ee30ec1ccc379bc56bb1694
austinbenedicto <austinbenedicto271@gmail.com>
2024-02-17 14:00:14 -0500

    the Note locations on the feild were added

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-LewZealand";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 59;
-  public static final String GIT_SHA = "dae12ff6203f2fc59a6612f2653e9b0091f3ac46";
-  public static final String GIT_DATE = "2024-02-15 18:29:03 EST";
+  public static final int GIT_REVISION = 60;
+  public static final String GIT_SHA = "b9c7d4f22cd0b9b0d836a219f01865fe61a83e2b";
+  public static final String GIT_DATE = "2024-02-17 11:03:41 EST";
   public static final String GIT_BRANCH = "task/getAprilTags";
-  public static final String BUILD_DATE = "2024-02-17 11:01:22 EST";
-  public static final long BUILD_UNIX_TIME = 1708185682788L;
+  public static final String BUILD_DATE = "2024-02-17 13:58:04 EST";
+  public static final long BUILD_UNIX_TIME = 1708196284953L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
