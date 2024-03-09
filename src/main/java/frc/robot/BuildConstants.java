930b1b367ced059951d61b7190f4973a4a032aa5
indoorfish01 <broland607@gmail.com>
2024-03-09 15:40:20 -0500

    climb test

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-LewZealand";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 149;
-  public static final String GIT_SHA = "ec5c8ef41000c94c2be69bc1e57d9ca4f99ebea0";
-  public static final String GIT_DATE = "2024-03-09 13:12:18 EST";
-  public static final String GIT_BRANCH = "develop";
-  public static final String BUILD_DATE = "2024-03-09 14:03:31 EST";
-  public static final long BUILD_UNIX_TIME = 1710011011181L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 150;
+  public static final String GIT_SHA = "f4bc0dc9f068092799315142f16599d54b2bf361";
+  public static final String GIT_DATE = "2024-03-09 14:17:27 EST";
+  public static final String GIT_BRANCH = "task/implement_climb";
+  public static final String BUILD_DATE = "2024-03-09 15:38:20 EST";
+  public static final long BUILD_UNIX_TIME = 1710016700826L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
