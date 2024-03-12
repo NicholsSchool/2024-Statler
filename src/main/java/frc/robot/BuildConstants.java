8fa88ab27e4561428a363f132985393038d62b5c
Anthony Honeine <anthonyhoneine@hotmail.com>
2024-03-12 17:11:08 -0400

    ig yeah, sorry james

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-LewZealand";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 181;
-  public static final String GIT_SHA = "16406f382cafc51d72fce52d97f410f67f60b7a0";
-  public static final String GIT_DATE = "2024-03-10 16:05:46 EDT";
-  public static final String GIT_BRANCH = "develop";
-  public static final String BUILD_DATE = "2024-03-10 16:07:59 EDT";
-  public static final long BUILD_UNIX_TIME = 1710101279787L;
+  public static final int GIT_REVISION = 184;
+  public static final String GIT_SHA = "35449c0bcf662a53ed4c2f5449dc643f1cb3e2d3";
+  public static final String GIT_DATE = "2024-03-12 07:51:20 EDT";
+  public static final String GIT_BRANCH = "task/shuffleboard_outputs";
+  public static final String BUILD_DATE = "2024-03-12 16:55:22 EDT";
+  public static final long BUILD_UNIX_TIME = 1710276922661L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
