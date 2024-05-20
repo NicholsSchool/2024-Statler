8d72bacd64965d60d9e09c234f41711c159fc179
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-05-20 10:45:31 -0400

    updated wpilib and vendor libraries, initial commit

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -3,14 +3,14 @@ package frc.robot;
 /** Automatically generated file containing build version information. */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "2024-LewZealand";
+  public static final String MAVEN_NAME = "2024-Statler";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 251;
-  public static final String GIT_SHA = "7076a7db7947237fe6b47617848107437a5c6c1c";
-  public static final String GIT_DATE = "2024-04-04 14:22:09 EDT";
-  public static final String GIT_BRANCH = "MVRDayOne";
-  public static final String BUILD_DATE = "2024-04-04 15:00:47 EDT";
-  public static final long BUILD_UNIX_TIME = 1712257247750L;
+  public static final int GIT_REVISION = 264;
+  public static final String GIT_SHA = "25b512d0300f09dbd83cc746d285de125ac147c5";
+  public static final String GIT_DATE = "2024-04-09 07:27:15 EDT";
+  public static final String GIT_BRANCH = "MVRDay3";
+  public static final String BUILD_DATE = "2024-05-20 10:43:49 EDT";
+  public static final long BUILD_UNIX_TIME = 1716216229024L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
