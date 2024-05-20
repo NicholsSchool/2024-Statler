2f8e41a8b46a3b071f5bdb45568f6a8642e589d0
AnthonyHoneine <anthonyhoneine@hotmail.com>
2024-05-20 12:14:35 -0400

    ready to purge lew zealand only stuff

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2024-Statler";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 264;
-  public static final String GIT_SHA = "25b512d0300f09dbd83cc746d285de125ac147c5";
-  public static final String GIT_DATE = "2024-04-09 07:27:15 EDT";
+  public static final int GIT_REVISION = 265;
+  public static final String GIT_SHA = "8d72bacd64965d60d9e09c234f41711c159fc179";
+  public static final String GIT_DATE = "2024-05-20 10:45:31 EDT";
   public static final String GIT_BRANCH = "MVRDay3";
-  public static final String BUILD_DATE = "2024-05-20 10:43:49 EDT";
-  public static final long BUILD_UNIX_TIME = 1716216229024L;
-  public static final int DIRTY = 1;
+  public static final String BUILD_DATE = "2024-05-20 12:14:06 EDT";
+  public static final long BUILD_UNIX_TIME = 1716221646327L;
+  public static final int DIRTY = 0;
 
   private BuildConstants() {}
 }
